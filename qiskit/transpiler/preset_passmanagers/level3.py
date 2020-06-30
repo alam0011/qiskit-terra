f76cdbbdb7af3142e48670c150e40c81f25a398b
Ali Javadi <ajavadia@princeton.edu>
2020-06-30 02:56:30 -0400

    add approximate decomposition

--- qiskit/transpiler/preset_passmanagers/level3.py
+++ qiskit/transpiler/preset_passmanagers/level3.py
@@ -53,6 +53,7 @@ from qiskit.transpiler.passes import ConsolidateBlocks
 from qiskit.transpiler.passes import UnitarySynthesis
 from qiskit.transpiler.passes import ApplyLayout
 from qiskit.transpiler.passes import CheckCXDirection
+from qiskit.transpiler.passes import Approx2qDecompose
 
 from qiskit.transpiler import TranspilerError
 
@@ -92,6 +93,7 @@ def level_3_pass_manager(pass_manager_config: PassManagerConfig) -> PassManager:
     translation_method = pass_manager_config.translation_method or 'translator'
     seed_transpiler = pass_manager_config.seed_transpiler
     backend_properties = pass_manager_config.backend_properties
+    synthesis_fidelity = pass_manager_config.synthesis_fidelity
 
     # 1. Unroll to 1q or 2q gates
     _unroll3q = Unroll3qOrMore()
@@ -168,6 +170,7 @@ def level_3_pass_manager(pass_manager_config: PassManagerConfig) -> PassManager:
         Collect2qBlocks(),
         ConsolidateBlocks(basis_gates=basis_gates),
         UnitarySynthesis(basis_gates),
+        Approx2qDecompose(fidelity=synthesis_fidelity),
         Optimize1qGates(basis_gates),
         CommutativeCancellation(),
     ]
