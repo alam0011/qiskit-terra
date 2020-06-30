be872722498c78b2671c279cea06d4d7fc948878
Ali Javadi <ajavadia@princeton.edu>
2020-06-30 01:20:58 -0400

    level 3: optimize after direction fixup

--- qiskit/transpiler/preset_passmanagers/level3.py
+++ qiskit/transpiler/preset_passmanagers/level3.py
@@ -213,7 +213,7 @@ def level_3_pass_manager(pass_manager_config: PassManagerConfig) -> PassManager:
     if coupling_map and not coupling_map.is_symmetric:
         pm3.append(_direction_check)
         pm3.append(_direction, condition=_direction_condition)
-    pm3.append(_reset)
+    pm3.append(_depth_check + _opt, do_while=_opt_control)
     if scheduling_method:
         pm3.append(_scheduling)
 
